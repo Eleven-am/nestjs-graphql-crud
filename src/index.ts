0d2bd49b12b392241979d4cbcb40ac405c9da7ce
Roy OSSAI <royossai@yahoo.com>
2025-06-06 20:12:00 +0200

    fix: simplify findMany argument handling in BaseCrudResolver

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { CrudModulesFactory } from './crudModulesFactory';
 export { PrismaDataProvider } from './providers/prisma/prismaDataProvider';
-export { CurrentPubSub } from './decorators';
+export { CurrentPubSub, createFindMany } from './decorators';
 export { PrismaFieldSelectionProvider } from './providers/prisma/prismaFieldSelectionProvider';
